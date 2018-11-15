3df41aa3c6918364a8915b3d0189d13418dd75ab
coder <mshawnlouw@gmail.com>
2018-11-15 11:00:39 +0200

    added a modal

--- public/js/waiter-dom.js
+++ public/js/waiter-dom.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
+  var elem = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elem);
 
   var instances = M.Dropdown.init(elems);
 
